9fa3f0f6c01acedba59e50a303a8d4cec81ee449
tik-217 <tigran.gabulyan.2001@mail.ru>
2024-11-25 22:22:33 +0300

    chore: adding nginx config in docker compose file

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-/*
+const http = require("http");
+const express = require("express");
 
-GEREKLİ PAKETLER YÜKLENİYOR...
+const app = express();
 
-*/
-var http = require("http");
-var express = require("express");
+app.set("port", process.env.PORT || 3005);
+app.set("views", __dirname + "/app/views");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/app/public"));
 
-var app = express();
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/app/views/index.html");
+});
 
-app.set("port", process.env.PORT || 3005); // GİRİŞ PORTU AYARLANDI
-app.set("views", __dirname + "/app/server/views"); // VIEW KLASÖRÜ TANITILDI
-app.set("view engine", "ejs"); // VIEW ENGINE AYARLANDI
-app.use(express.static(__dirname + "/app/public")); // KULLANICILAR TARAFINDAN ERİŞİLEBİLEN KLASÖR TANIMLANDI
-
-require("./app/routes")(app); // ROUTE DOSYASI ÇAĞIRILDI
-
-/*
-
-HTTP SERVER OLUŞTURULDU
-
-*/
 http.createServer(app).listen(app.get("port"), function () {
-  console.log("Sistem " + app.get("port") + " Portu Üzerinde Çalışıyor.");
+  console.log("System " + app.get("port") + " run in a port 3005");
 });
